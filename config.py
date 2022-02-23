ea50e22432766d7938c56e5ddd3c9f58fda2bde0
San4ezko26 <99359471+San4ezko26@users.noreply.github.com>
2022-02-23 17:31:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ admin=5259188215
 minimalka = 100
 maximalka = 20000
 vxodadmin='/админ' #Слово для входа в админ панель (Доступно только для админов)
-vxodworker='воркер' #Слово для входа в воркер панель (Доступно только для всех)
+vxodworker='/воркер' #Слово для входа в воркер панель (Доступно только для всех)
 zalety = -1001305864424
 bot_username = "BestEscortBot"
 qiwi_nick = "ESCORT26"
 chat_workers = "-1001791781996"
-text_workers = "1111111"
+text_workers = "https://telegra.ph/Manual-po-Luxury-Secret-12-19"
 
 
 
